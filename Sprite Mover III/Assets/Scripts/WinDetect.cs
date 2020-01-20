83dddf4a494d863f7e4a2f9083d6935ca50a6b81
Taylor McKenzie <xx20twinkiie14xx@gmail.com>
2020-01-20 15:33:49 -0700

    Made Scripts for Movement and Win Detection
    Trigger boxes were also placed for the piece detection.

---
+++ Sprite Mover III/Assets/Scripts/WinDetect.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinDetect : MonoBehaviour
+{
+    public GameObject trigger1;
+
+    public GameObject trigger2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject.FindGameObjectWithTag("Trigger1");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
