0f2b483a934528f3d1ea0b5adab52389b9bc8f3a
Taylor McKenzie <xx20twinkiie14xx@gmail.com>
2020-01-30 05:32:08 -0700

    Added Main Menu and Completed Piece Detection
    You can now win the game.

---
+++ Sprite Mover III/Assets/Scripts/DisplayMode.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class DisplayMode : MonoBehaviour
+{
+    public GameObject lighter;
+
+    public GameObject darker;
+
+    private bool darkEnable;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void ModeChange()
+    {
+        if (darkEnable == false)
+        {
+            lighter.GetComponent<TilemapRenderer>().sortingOrder = -1;
+            darker.GetComponent<TilemapRenderer>().sortingOrder = 0;
+            darkEnable = true;
+            FileKeep.DarkEnable = true;
+        }
+        else
+        {
+            lighter.GetComponent<TilemapRenderer>().sortingOrder = 0;
+            darker.GetComponent<TilemapRenderer>().sortingOrder = -1;
+            darkEnable = false;
+            FileKeep.DarkEnable = true;
+        }
+    }
+}
--- Sprite Mover III/Assets/Scripts/WinDetect.cs
+++ Sprite Mover III/Assets/Scripts/WinDetect.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.WSA;
+using UnityEngine.SceneManagement;
 
 public class WinDetect : MonoBehaviour
 {
@@ -9,11 +10,9 @@ public class WinDetect : MonoBehaviour
     public GameObject trigger2;
     public GameObject trigger3;
     public GameObject trigger4;
-    /*
-    private PieceDetect win;
-    private Piece2Detect win2;
-    private Piece3Detect win3;
-    private Piece4Detect win4;*/
+    public Movement move;
+
+    //private Transition myParent = GetComponent<Transition>();
 
     // Start is called before the first frame update
     void Start()
@@ -22,8 +21,8 @@ public class WinDetect : MonoBehaviour
         trigger2 = GameObject.FindGameObjectWithTag("Trigger2");
         trigger3 = GameObject.FindGameObjectWithTag("Trigger3");
         trigger4 = GameObject.FindGameObjectWithTag("Trigger4");
-
         
+
     }
 
     // Update is called once per frame
@@ -41,7 +40,22 @@ public class WinDetect : MonoBehaviour
 
         if (trigger1.GetComponent<PieceDetect>().activated1 == true && trigger2.GetComponent<Piece2Detect>().activated2 == true && trigger3.GetComponent<Piece3Detect>().activated3 == true && trigger4.GetComponent<Piece4Detect>().activated4 == true)
         {
+            StartCoroutine(waiter());
             Debug.Log("YOU WON YOU ACTUALLY WON");
+            waiter();
         }
+    }
+
+    IEnumerator waiter()
+    {
+        yield return new WaitForSeconds(1);
+        move.speed = 0;
+        yield return new WaitForSeconds(5);
+        LoadLevel("LevelSelect");
+    }
+
+    void LoadLevel(string level)
+    {
+        SceneManager.LoadScene(level);
     }
 }
